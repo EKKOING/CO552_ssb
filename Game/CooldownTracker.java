403a072dcad9f22fc3fef8df8c7106023ac362f6
Nicholas L <33159891+EKKOING@users.noreply.github.com>
2019-05-07 09:13:46 -0400

    Comments

--- Game/CooldownTracker.java
+++ Game/CooldownTracker.java
@@ -4,20 +4,29 @@ import java.util.TimerTask;
 /**
 Creates a cooldown timer that will set the state of a cooldown after a time period
 @author Nicholas Lorentzen
-@version 04/28/2019
+@version 05/07/2019
 */
-
 public class CooldownTracker {
+    /** Timer thread */
     Timer timer;
+    /** Name of CD being tracked */
     String whichCD;
+    /** Player whom the CD belongs to */
     Player myPlayer;
 
+    /**
+     * Constructs a new CD activator
+     * @param myPlayer Player to act upon
+     * @param ms Ms of CD time
+     * @param myCD CD to act upon
+     */
     public CooldownTracker(Player myPlayer, long ms, String myCD) {
         timer = new Timer();
         whichCD = myCD;
         timer.schedule(new RemindTask(), ms);
 	}
 
+    /** Thread to manage cooldown */
     class RemindTask extends TimerTask {
         public void run() {
             if(whichCD == "canWalk")
@@ -26,7 +35,7 @@ public class CooldownTracker {
             }
             if(whichCD == "canAttack")
             {
-                //Will update
+                myPlayer.canAttack = true;
             }
             timer.cancel();
         }
